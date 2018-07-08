dc9a92fffa8d4cd24512a2e8d56c164b869d3f06
Basico <espindolage@gmail.com>
2018-07-08 14:43:29 -0400

    Init 2018

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-$('body').show();
-
-$(document).on('click', '.navbar a, .goto', function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - $('.navbar').height() -60
-    }, 500);
-});
